7d8eec61e80ed2808a4bafacf8f919b80a63db51
Piotr Surowiec <piotrek.scgn@gmail.com>
2023-12-13 17:08:05 +0100

    Removes not needed component and images

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import "./Home.css";
 import "../PrimaryButton/PrimaryButton";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import { descirpiotn } from "../../constants/description";
 
 const Home = ({ navigate }) => {
   /* Component to display non-authenticated home page.
@@ -18,6 +19,7 @@ const Home = ({ navigate }) => {
   return (
     <div className="container primary-background-colour">
       <h1 className="primary-heading" data-cy="head-content">Roam Mates</h1>
+      <p className="description">{descirpiotn}</p>
       <PrimaryButton text="Login" onClick={navigateToLogin} />
       <PrimaryButton text="Sign Up" onClick={navigateToSignUp} />
     </div>
