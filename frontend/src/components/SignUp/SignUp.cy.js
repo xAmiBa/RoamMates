d6ea913d3e0553e32026771d8e8286a0e4290939
David O'Donnell <dave@soundmotives.net>
2023-12-13 15:46:24 +0000

    updated SignUp test

--- frontend/src/components/SignUp/SignUp.cy.js
+++ frontend/src/components/SignUp/SignUp.cy.js
@@ -6,6 +6,5 @@ describe("Signing up", () => {
   it("After sign up, redirects to the /login page", () => {
     cy.mount(<SignUp navigate={navigate}/>)
     cy.get('[data-cy="signup-heading"]').should('contain.text', "Sign Up")
-
     })
   })
