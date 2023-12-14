85ebc61be556b7fad365fa4337d7b33d9efdfb92
David O'Donnell <dave@soundmotives.net>
2023-12-14 16:33:54 +0000

    added SideBar component

--- frontend/src/components/UserList/UserList.js
+++ frontend/src/components/UserList/UserList.js
@@ -2,6 +2,8 @@ import { useState } from "react"
 import'./UserList.css'
 import UserCard from "../UserCard/UserCard"
 import users from "../../constants/userList"
+import SideBar from "../SideBar/SideBar"
+
 
 const UserList = (props) => {
 /*
@@ -36,6 +38,8 @@ Children:
 // TODO: Add API request to change state of the UserList.
 
     return(
+        <>
+        <SideBar navigate={props.navigate}/>
         <div className="container primary-background-colour">
             <h1 className="primary-heading" data-cy="test-heading">{heading}</h1>
             <div className="user-list-container">
@@ -44,6 +48,7 @@ Children:
                 ))}
                 </div>
         </div>
+        </>
     )
 }
 
