13e756202399883dd16c03164191930c86852e3a
David O'Donnell <dave@soundmotives.net>
2023-12-14 12:52:02 +0000

    added UserCard test

--- frontend/src/components/UserCard/UserCard.js
+++ frontend/src/components/UserCard/UserCard.js
@@ -14,14 +14,14 @@ const user = props.user
 //TODO: Add link to the 'User Detail view"
     return(
         <div onClick={goToUserDetail} className="user-card shadow-effect">
-            <span className="user-info primary-text-colour">{user.username}</span>
+            <span className="user-info primary-text-colour" data-cy="test-UserName">{user.username}</span>
             <div className="image-container">
                 <img src="avatar.webp"/>
             </div>
             <div>
-                <span className="user-info primary-text-colour">{user.age}</span>
-                <span className="user-info primary-text-colour">{user.gender}</span>
-                <p>{user.bio}</p>
+                <span className="user-info primary-text-colour" data-cy="test-age">{user.age}</span>
+                <span className="user-info primary-text-colour" data-cy="test-gender">{user.gender}</span>
+                <p data-cy="test-bio">{user.bio}</p>
             </div>
         </div>
     )
