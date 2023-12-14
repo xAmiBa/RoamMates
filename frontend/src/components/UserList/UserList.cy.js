d16b101f2aac7b08d6557b74c4111ae84048ca7e
David O'Donnell <dave@soundmotives.net>
2023-12-14 15:31:12 +0000

    added test for UserList component

--- frontend/src/components/UserList/UserList.cy.js
+++ frontend/src/components/UserList/UserList.cy.js
@@ -6,4 +6,15 @@ describe("UserList", () => {
     cy.mount(<UserList navigate={navigate} componentVersion='home'/>);
     cy.get('[data-cy="test-heading"]').should("contain.text", "Find Your Roam Mates!");
   });
+
+  it("renders User List with correct heading", () => {
+    cy.mount(<UserList navigate={navigate} componentVersion='requests'/>);
+    cy.get('[data-cy="test-heading"]').should("contain.text", "Requests");
+  });
+  
+    it("renders User List with correct heading", () => {
+    cy.mount(<UserList navigate={navigate} componentVersion='matches'/>);
+    cy.get('[data-cy="test-heading"]').should("contain.text", "My Matches");
+  });
+
 });
