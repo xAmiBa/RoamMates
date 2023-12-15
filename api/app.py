314da8320603d564a64d79ed24534a52f4841822
xAmiBa <aminaba666@gmail.com>
2023-12-15 12:28:16 +0000

    changes after review

--- api/app.py
+++ api/app.py
@@ -18,48 +18,59 @@ Route: /users/add
 Request: POST
 [Signup, adds user to Users table]
 """
+
+
 @app.route("/users/add", methods=["POST"])
 def user_signup():
     connection = get_flask_database_connection(app)
     users = UserRepository(connection)
 
-    username = request.form.get('username')
-    password = request.form.get('password')
-    email = request.form.get('email')
+    username = request.form.get("username")
+    password = request.form.get("password")
+    email = request.form.get("email")
     print("EMAIL PASSED:", email)
 
     # Check if user email is unique
     if users.find_by_email(email) == []:
         users.add(User(None, username, password, email))
-        response = app.response_class(response=json.dumps({"message": "OK!"}), status=200)
-  
+        response = app.response_class(
+            response=json.dumps({"message": "OK!"}), status=200
+        )
+
     else:
         print("User already exists:")
-        response = app.response_class(response=json.dumps({"message": "Credentials error"}), status=401)
+        response = app.response_class(
+            response=json.dumps({"message": "Credentials error"}), status=401
+        )
 
     return response
+
+
 """
 Route: /users/authentication
 Request: POST
 [verifies that username matches password and creates a token]
 """
+
+
 @app.route("/users/authentication", methods=["POST"])
 def user_login():
     connection = get_flask_database_connection(app)
-    users = UserRepository(connection)
+    users_repo = UserRepository(connection)
 
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form.get("email")
+    password = request.form.get("password")
 
-    if users.check_login_details(email, password) is True:
-        user = users.find_by_email(email)
+    if users_repo.check_login_details(email, password):
+        user = users_repo.find_by_email(email)
         token = token_generator(user.id)
-        response = app.response_class(response=json.dumps({
-            "message": "OK!",
-            "token": token
-            }),status=200)
+        response = app.response_class(
+            response=json.dumps({"message": "OK!", "token": token}), status=200
+        )
         return response
-    return app.response_class(response=json.dumps({"message": "Invalid credentials"}), status=401)
+    return app.response_class(
+        response=json.dumps({"message": "Invalid credentials"}), status=401
+    )
 
 
 """
@@ -69,7 +80,5 @@ Request:
 """
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
-
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
