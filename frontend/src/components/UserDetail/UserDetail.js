7c200734f1a8473025ef5a396817ca1191c6496b
David O'Donnell <dave@soundmotives.net>
2023-12-15 15:18:38 +0000

    Created UserDetail html structure

--- frontend/src/components/UserDetail/UserDetail.js
+++ frontend/src/components/UserDetail/UserDetail.js
@@ -3,18 +3,18 @@ import "../UserDetail/UserDetail.css"
 
 const UserDetail = ({navigate}) => {
     return(
-        <div className="container primary-background-colour ">
+        <div className="profile-detail-container primary-background-colour">
             <div className="row">
                 <div className="col">
-                    <div className="image-container">
+                    <div className="image-container profile-image">
                         <img src="avatar.webp"/>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col bio-container">
                     Bio
                 </div>
             </div>
-            <div className="row">
+            <div className="row preferences-container">
                 Preferences
             </div>
         </div>
