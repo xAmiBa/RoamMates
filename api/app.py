afbdb5bfb13f5372bffa6948499ffc5b86199002
xAmiBa <aminaba666@gmail.com>
2024-01-03 13:55:20 +0000

    BE: Signup query modified so it updates users and profiles table FE: connected to BE and tested in a browser, new user and profile added successfuly BE: Signup deleted from auth routed and moved to users routes as in the API plan

--- api/app.py
+++ api/app.py
@@ -5,6 +5,7 @@ import secrets
 from routes.profile import apply_profile_routes
 from routes.auth import apply_auth_routes
 from routes.requests import apply_request_routes
+from routes.user import apply_user_routes
 
 
 # Create a new Flask app
@@ -15,6 +16,7 @@ app.secret_key = secrets.token_hex(32)
 apply_profile_routes(app)
 apply_auth_routes(app)
 apply_request_routes(app)
+apply_user_routes(app)
 
 
 if __name__ == "__main__":
---
+++ api/routes/user.py
@@ -0,0 +1,40 @@
+from flask import request, jsonify, session
+from token_config import token_checker, token_generator
+from lib.database_connection import get_flask_database_connection
+
+from lib.User_repository import UserRepository
+from lib.User import User
+
+def apply_user_routes(app):
+    """User Router."""
+
+    @app.route("/users/add", methods=["POST"])
+    def user_signup():
+        """
+        Route: /users/add
+        Request: POST
+        Signup, adds user to Users table.
+        """
+
+        connection = get_flask_database_connection(app)
+        user_repo = UserRepository(connection)
+
+        data = request.get_json()
+        email = data.get("email")
+        username = data.get("username")
+        password = data.get("password")
+
+        # Check if user email is unique
+        if user_repo.find_by_email(email):
+            print("User already exists:")
+            response = jsonify({"message": "Credentials error"})
+            response.status_code = 401
+
+        else:
+            new_user = User(None, username, password, email)
+            user_repo.add(new_user)
+            response = jsonify({"message": "OK!"})
+            response.status_code = 200
+
+        return response
+
