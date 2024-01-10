43e37d3479356649a6a489f9c9d5d52c4f9422f9
xAmiBa <aminaba666@gmail.com>
2024-01-10 16:32:31 +0000

    preference form isolated in seperate component

--- frontend/src/components/Settings/Settings.js
+++ frontend/src/components/Settings/Settings.js
@@ -3,6 +3,7 @@ import "../Settings/Settings.css"
 import { useGetSingleUser } from "../../services/getSingleUser";
 import { useState, useEffect } from "react";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import PreferencesForm from "../PreferencesForm/PreferencesForm";
 
 const Settings = ({ navigate }) => {
     // Sets up and inputs ValueState
@@ -30,18 +31,7 @@ const Settings = ({ navigate }) => {
             });
         }, [userId]);
 
-    const onChange = (event) => {
-        setValues({ ...values, [event.target.name]: event.target.value });
-    };
-
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const apiSingleUserDataUrl = process.env.REACT_APP_PREFERENCES_FOR_USER_ID;
-    //     useGetSingleUser(
-    //         window.localStorage.getItem("token"),
-            
-    //     )
-    // };
+    
     return (
         <div className="settings-container">
             <UserDetail navigate={navigate} />
@@ -50,73 +40,8 @@ const Settings = ({ navigate }) => {
                 <div className="profile-settings-container">
                     {/* TODO:  */}
                 </div>
-
-                <div className="preference-settings-container">
-                <form onSubmit={ useEffect }>
-
-                    <div className="preference-age">
-                        <label for="age_slot">Age</label>
-                        <select id="age_slot" name="age_slot">
-                            <option value="current-preference">{values.preferences.age_slot}</option>
-                            <option value="[18, 24]">18-24</option>
-                            <option value="[25, 30]">25-30</option>
-                            <option value="[30, 100]">30+</option>
-                        </select>
-                    </div><br/>
-
-                    <div className="preference-gender">
-                    <label for="gender">Gender</label>
-                        <select id="gender" name="gender">
-                            <option value="current-preference">{values.preferences.gender}</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="other">Other</option>
-                        </select>
-                    </div><br/>
-
-                    <div className="prefernce-continent">
-                        <label for="continent">Continent</label>
-                        <select id="continent" name="continent">
-                            <option value="current-preference">{values.preferences.continent}</option>
-                            <option value="Africa">Africa</option>
-                            <option value="Antarctica">Antarctica</option>
-                            <option value="Asia">Asia</option>
-                            <option value="Europe">Europe</option>
-                            <option value="North America">North America</option>
-                            <option value="Oceania">Oceania</option>
-                            <option value="South America">South America</option>
-                        </select>
-                    </div><br/>
-
-                    <div className="preference-season">
-                        <label for="season">Season</label>
-                        <select id="season" name="season">
-                            <option value="current-preference">{values.preferences.season}</option>
-                            <option value="spring">Spring</option>
-                            <option value="summer">Summer</option>
-                            <option value="autumn">Autumn</option>
-                            <option value="winter">Winter</option>
-                        </select>
-                    </div><br/>
-
-                    <div className="preference-category">
-                        <label for="category">Category</label>
-                        <select id="category" name="category">
-                            <option value="current-preference">{values.preferences.category}</option>
-                            <option value="mountains">Mountains</option>
-                            <option value="beach">Beach</option>
-                            <option value="resort">Resort</option>
-                            <option value="city">City</option>
-                            <option value="nature">Nature</option>
-                            <option value="sport">Sport</option>
-                        </select>
-                    </div><br/>
-                    <div className="button-container">
-                        <PrimaryButton text="Set up new preferences" id="set-up-button" />
-                    </div>
+                <PreferencesForm {...values} {...setValues}/>
                 
-                </form>
-                </div>
 
             </div>
         </div>
--- frontend/src/services/setupPreferences.js
+++ frontend/src/services/setupPreferences.js
@@ -8,5 +8,33 @@ Params:
     @param continent - string - continent value.
     @param season - string - season value.
     @param category - string - category value.
+    @param setFormError - funct to change state of Error.
+
 */
 
+const setupPreferences = async (apiUrl, age_slot, gender, continent, season, category, setFormError) => {
+    let response = await fetch(apiUrl, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": window.localStorage.getItem("token")
+        },
+        body: JSON.stringify({
+            age_slot: age_slot,
+            gender: gender,
+            continent: continent,
+            season: season,
+            category: category
+        }),
+    });
+
+    if (response.status != 200) {
+        setFormError("Something went wrong!")
+    } else {
+        const data = await response.json();
+        window.localStorage.setItem("token", data.token);
+        window.localStorage.setItem("id", data.user_id)
+    }
+}
+
+export default setupPreferences;
