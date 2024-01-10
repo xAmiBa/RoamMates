4e26c3db301f965d59eb2511ba4b33ee1ee47c09
Daniel <dankrusz@hotmail.co.uk>
2024-01-10 15:55:40 +0000

    adding getSingleUser

---
+++ frontend/src/services/getSingleUser.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+/** 
+Service to get a single user from API.
+
+Params:
+    @param token = Auth token
+    @param setSingleUser = function to set a single users
+    @param setError = functions to set list of errors
+    @param userId - id of user we want to view.
+*/
+const useGetSingleUser = (token, setSingleUser, setError, userId) => {
+    useEffect(() => {
+    const apiUrl = process.env.REACT_APP_PROFILES_DETAIL_BASE_URL + userId;
+
+    fetch(apiUrl, {
+        headers: {
+        Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => setSingleUser(data))
+        .catch((error) => {
+        setError(error);
+        });
+    }, [userId]);
+};
+
+export default useGetSingleUser;
